4016be38476fd29f3f6a75f09ad4d0f1422b7dce
Francisco Bento <francisco.bento.jr@hotmail.com>
2016-12-21 00:47:17 -0200

    Add player and ball drawing

--- jogo/pong.asm
+++ jogo/pong.asm
@@ -1,10 +1,29 @@
+# 512x512 || 4x4
+
+.data 0x10000000
+	color: .word 0xff
+	width: .word 128
+ 	height: .word 128
+ 	playerSize: .word 10
+ 	startPoint1: .word 63
+ 	startPoint2: .word 63
+ 	ballX: .word 63
+ 	ballY: .word 63
 .text
 main:
 	lui $10, 0x1001
-	addi $21, $0, 64
-	addi $22, $0, 64
-	addi $11, $0, 0xffffff
+	lw $11, color
+	lw $21, width
+	lw $22, height
+	lw $23, playerSize
+	lw $26, startPoint1
+	lw $27, startPoint2
+	lw $28, ballX
+	lw $29, ballY
 	jal drawBorders
+	jal drawPlayer1
+	jal drawPlayer2
+	jal drawBall
 	j fim
 
 drawBorders:
@@ -23,15 +42,68 @@ drawingBorders:
 	jal drawPixel
 	addi $25, $25, 1
 	j drawingBorders
-
+	
 endDrawBorders:
 	addi $31, $8, 0
 	jr $31
 
-fim:
-	nop
-	j fim
+drawPlayer1:
+	addi $8, $31, 0
+	add $4, $0, $0
+	sub $5, $26, $23
+	add $6, $26, $23
+	j drawingPlayer
+	
+drawPlayer2:
+	addi $8, $31, 0
+	subi $4, $21, 2
+	sub $5, $27, $23
+	add $6, $27, $23
+	j drawingPlayer
 
+drawingPlayer:
+	beq $5, $6, endDrawingPlayer
+	add $12, $5, $0
+	add $13, $4, $0
+	jal drawPixel
+	add $13, $13, 1
+	jal drawPixel
+	addi $5, $5, 1
+	j drawingPlayer
+	
+endDrawingPlayer:
+	addi $31, $8, 0
+	jr $31
+
+drawBall:
+	add $8, $31, $0
+	add $12, $28, $0
+	add $13, $29, $0
+	jal drawPixel
+	subi $12, $12, 1
+	jal drawPixel
+	add $12, $28, $0
+	addi $12, $12, 1
+	jal drawPixel
+	add $12, $28, $0
+	subi $13, $13, 1
+	jal drawPixel
+	add $13, $29, $0
+	addi $13, $13, 1
+	jal drawPixel
+	add $13, $29, $0
+	addi $12, $12, 1
+	addi $13, $13, 1
+	jal drawPixel
+	subi $12, $12, 2
+	jal drawPixel
+	subi $13, $13, 2
+	jal drawPixel
+	addi $12, $12, 2
+	jal drawPixel
+	add $31, $8, $0
+	jr $31
+			
 drawPixel:
 	add $9, $31, $0
 	mul $14, $22, 4
@@ -42,3 +114,7 @@ drawPixel:
 	sw $11, 0($14)
 	add $31, $9, $0
 	jr $31
+	
+fim:
+	li $v0,10
+	syscall
